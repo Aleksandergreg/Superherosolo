12544bed083c33eec292c76e4e10ccb5c674ad5d
Aleksandergreg <alek3691@stud.kea.dk>
2023-09-19 08:56:35 +0200

    v0.1 test method getSearchmatches and addSuperhero

--- Test/DatabaseTest.java
+++ Test/DatabaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.xml.crypto.Data;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,15 +28,36 @@ class DatabaseTest {
     }
 
     @Test
-    void getSearchMatches() {
+    void getSearchMatchesWithOne() {
         s1 = new Superhero("Spiderman", "Aner det ikke","Edderkoppesværd", 1990, true, 1000);
         database.getSuperheroesArrayList().add(s1);
         //Arrange
         String expectedName = "Spiderman";
-        //Aact
+        //Act
         String actualName = s1.getSuperheroName();
         //Assert
         assertEquals(expectedName, actualName);
+    }
+    @Test
+    void getSearchMatches_NoMatches() {
+        // Arrange
+        int expectedSize = 0;
+        // Act
+        List<Superhero> searchMatches = database.getSearchMatches();
+
+        // Assert
+        assertEquals(expectedSize, searchMatches.size());
+    }
+    @Test
+    void getSearchMatches(){
+        s1 = new Superhero("Spiderman", "Aner det ikke","Edderkoppesværd", 1990, true, 1000);
+        s2 = new Superhero("Ironman", "Vides ikke","Jern", 1980, true, 300);
+        s3 = new Superhero("Superman", "Clark Kent", "Laser", 1983, true, 183);
+
+        database.getSearchMatches().addAll(List.of(s1,s2,s3));
+        List<Superhero> searchMatches = database.getSearchMatches();
+
+        assertEquals(searchMatches.size(), 3);
     }
 
     @Test
